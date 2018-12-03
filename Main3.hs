5d568717693ce7302884761385215b12b13b99e8
Shae Erisson <shae@scannedinavian.com>
2018-12-03 01:09:38 -0500

    day3 part 1

--- Main3.hs
+++ Main3.hs
@@ -2,44 +2,59 @@
 module Main where
 
 import Data.List
+import Data.Text
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Attoparsec.Text hiding (take, takeWhile)
 
 main :: IO ()
-main = do input <- Prelude.readFile "input2.txt"
-          print "day 2 part 1"
-          let ls = lines input
-          print $ check 2 ls * check 3 ls
-          print "day 2 part 2"
-          print $ map fst $ filter (uncurry (==)) (uncurry zip $ diff ls)
+main = do input <- TIO.readFile "input3.txt"
+          let ls = T.lines input
+          print "day 3 part 1"
+          let claims = fromRight (parseM input :: Either String [Claim])
+          let coords = Prelude.concatMap (getCoords . conv) claims
+          let counts = Prelude.foldl myIns emptyCmap coords
+          print $ Prelude.length $ M.toList $ M.filter (> 1) counts
+          -- print $ Prelude.length counts
+          -- 120813 is too high
+          print "hi"
 
-test = [ "abcdef",
-         "bababc",
-         "abbcde",
-         "abcccd",
-         "aabcdd",
-         "abcdee",
-         "ababab"
-       ]
+data Claim = C Int Int Int Int Int deriving (Show, Eq, Ord)
 
-check n ls = length $ filter ((> 0) . length) $ (filter ((== n) . length)) <$> (group . sort <$> ls)
+-- "#11 @ 569,720: 28x29"
+pClaim = C <$ char '#'
+  <*> decimal
+  <* string " @ "
+  <*> decimal
+  <* char ','
+  <*> decimal
+  <* string ": "
+  <*> decimal
+  <* char 'x'
+  <*> decimal
 
-test2 = ["abcde",
-         "fghij",
-         "klmno",
-         "pqrst",
-         "fguij",
-         "axcye",
-         "wvxyz"
-        ]
 
-diff :: [String] -> (String,String)
-diff [] = ("","")
-diff (x:xs) = let
-  dists = (distance x) <$> xs
-  f = filter ((==1) . fst) dists
-  in
-    if (length f) == 1 then
-      (x, snd $ head f)
-    else diff xs
+fromRight (Right x) = x
 
-distance :: String -> String -> (Int,String)
-distance x y = (length $ filter (not . id) $ zipWith (==) x y, y)
+test :: Text
+test = "#11 @ 569,720: 28x29"
+
+parseM = parseOnly (pClaim `sepBy` char '\n')
+
+-- which inches have overlap?
+starts (C _ x y _ _) = (x,y)
+
+lengths (C _ _ _ w h) = (w,h)
+
+-- Data.Map to the rescue?
+
+emptyCmap :: M.Map (Int,Int) Int
+emptyCmap = M.empty
+
+getCoords (x1,y1,x2,y2) = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
+
+myIns m k = M.insertWith (+) k 1 m
+
+-- convert Claim to (x1,y1,x2,y2)
+conv (C _ x1 y1 xoff yoff) = (x1+1, y1+1, x1+xoff, y1+yoff)
