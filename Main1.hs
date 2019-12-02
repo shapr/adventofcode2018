e3a730df25c06da4fccc53c15187031f315353ce
Shae Erisson <shae@scannedinavian.com>
2019-12-01 21:35:26 -0500

    clean out bad code

--- Main1.hs
+++ Main1.hs
@@ -1,50 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Text.IO as TIO
-import qualified Data.Map.Strict as Map
-import Data.List (sort, group, tails)
-import qualified Data.Text as T
-import Data.Attoparsec.Text hiding (take, takeWhile)
-
+import           Data.Attoparsec.Text hiding (take, takeWhile)
+import qualified Data.HashSet         as S
+import           Data.List            (foldl', group, sort, tails)
+import qualified Data.Text            as T
+import           Data.Text.IO         as TIO
 
 main :: IO ()
-main = do input <- TIO.readFile "input1.txt"
+main = do input <- TIO.readFile "day1"
           print "day 1 part 1"
           print input
           let res = parseOnly (double `sepBy` char '\n') input
           print $ sum $ fromRight res
           print "day 2 part 2"
-          print $ "do the tests work? " <> show tests
-          print $ uglyhack (fromRight res ++  fromRight res)
-
-
-pNum = decimal
+          print $ "do the tests work? " <> show tests'
+          print $ checkit S.empty (scans $ fromRight res)
 
 fromRight (Right a) = a
 
--- ugly hack it
-hack ns = filter ((> 1) . length) (group $ sort ns)
-
--- sublists ns =  reverse $ tails $ reverse ns
-sublists ns = flip take ns <$> [1..]
-
-
-takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
-takeWhileInclusive _ [] = []
-takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs
-                                         else []
-
-uglyhack ns = take 1 $ concat $ hack <$> sublists scans
-  where scans = scanl (+) 0 (cycle ns)
+checkit s []     = []
+checkit s (a:as) = if S.member a s then [a] else checkit (S.insert a s) as
 
 scans ns = scanl (+) 0 (cycle ns)
 
-tests = all id [
-  uglyhack [7, 7, -2, -7, -4] == [[14,14]],
-  uglyhack [3, 3, 4, -2, -4] == [[10,10]],
-  uglyhack [-6, 3, 8, 5, -6] == [[5,5]],
-  uglyhack [1,-1] == [[0,0]]
-  ]
-
---
+tests' = and [
+          checkit S.empty (scans [7, 7, -2, -7, -4] :: [Int]) == [14]
+         , checkit S.empty (scans [3, 3, 4, -2, -4] :: [Int]) == [10]
+         , checkit S.empty (scans [-6, 3, 8, 5, -6] :: [Int]) == [5]
+         , checkit S.empty (scans [1,-1] :: [Int]) == [0]
+         ]
